c5c044f3380603a919d690264995b2506cc5a64c
Debian <bitnami@ip-172-26-1-226.ap-northeast-1.compute.internal>
2021-02-23 07:22:36 +0000

    first commit

--- frontend/src/tutorial/App.vue
+++ frontend/src/tutorial/App.vue
@@ -1,19 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Tutorial"/>
-  </div>
+  <v-app id="inspire">
+    <div class="text-center">
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="primary"
+      >
+        <v-icon dark>
+          mdi-minus
+        </v-icon>
+      </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="pink"
+      >
+        <v-icon dark>
+          mdi-heart
+        </v-icon>
+      </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        color="indigo"
+      >
+        <v-icon dark>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        color="teal"
+      >
+        <v-icon dark>
+          mdi-format-list-bulleted-square
+        </v-icon>
+      </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        large
+        color="cyan"
+      >
+        <v-icon dark>
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        large
+        color="purple"
+      >
+        <v-icon dark>
+          mdi-android
+        </v-icon>
+      </v-btn>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data: () => ({
+    }),
+    props: {
+        source: String
+    },
+    methods: {
+    }
+};
 </script>
 
 <style>
--- frontend/src/tutorial/main.js
+++ frontend/src/tutorial/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+const vuetify = new Vuetify()
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+//    router,
+    vuetify,
+    render: h => h(App),
 }).$mount('#app')
