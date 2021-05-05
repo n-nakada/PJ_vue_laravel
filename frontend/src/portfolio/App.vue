670bc4018701d09414981d82b95f71cd0646eace
Debian <bitnami@ip-172-26-1-226.ap-northeast-1.compute.internal>
2021-05-05 05:24:48 +0000

    画面レイアウト作成（ヘッダ・フッタ・サイドバー）

--- frontend/src/portfolio/App.vue
+++ frontend/src/portfolio/App.vue
@@ -1,12 +1,138 @@
 <template>
     <v-app>
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            clipped
+        >
+            <v-container>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="title grey--text text--darken-2">
+                            Navigation lists
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list dense nav>
+                    <v-list-group
+                        v-for="nav_list in nav_lists"
+                        :key="nav_list.name"
+                        :prepend-icon="nav_list.icon"
+                        no-action
+                        :append-icon="nav_list.lists ? undefined : ''"
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                        <v-list-item v-for="list in nav_list.lists" :key="list">
+                            <v-list-item-content>
+                                <v-list-item-title>{{ list }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                </v-list>
+            </v-container>
+        </v-navigation-drawer>
+        <v-app-bar
+            color="grey darken-3"
+            dark
+            app
+            clipped-left
+        >
+            <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+            <v-toobar-title>Laravel＋Vue ポートフォリオ</v-toobar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-menu offset-y>
+                    <template v-slot:activator="{on}">
+                        <v-btn
+                            v-on="on"
+                            text
+                        >
+                            <v-icon class="mr-1">mdi-account-circle</v-icon>
+                            プロファイル
+                            <v-icon>mdi-menu-down</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-subheader>Get help</v-subheader>
+                        <v-list-item
+                            v-for="support in supports"
+                            :key="support.name"
+                        >
+                            <v-list-item-icon>
+                                <v-icon>{{ support.icon }}</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ support.name }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </v-toolbar-items>
+        </v-app-bar>
         <router-view />
+        <v-footer
+            color="grey darken-3"
+            dark
+            app
+        >
+            Laravel＋Vue ポートフォリオ
+        </v-footer>
     </v-app>
 </template>
 
 <script>
 export default {
-    name: 'App'
+    name: 'App',
+    data() {
+        return {
+            drawer: null,
+            supports: [
+                { name: 'Consulting and suppourt', icon: 'mdi-vuetify' },
+                { name: 'Discord community', icon: 'mdi-discord' },
+                { name: 'Report a bug', icon: 'mdi-bug' },
+                { name: 'Github issue board', icon: 'mdi-github' },
+                { name: 'Stack overview', icon: 'mdi-stack-overflow' },
+                { name: 'Logout', icon: 'mdi-logout' }
+            ],
+            nav_lists: [{
+                name: 'Getting Started',
+                icon: 'mdi-vuetify',
+                lists: [
+                    'Quick Start',
+                    'Pre-made layouts'
+                ]
+            },{
+                name: 'Customization',
+                icon: 'mdi-cogs'
+            },{
+                name: 'Styles & animations',
+                icon: 'mdi-palette',
+                lists: [
+                    'Colors',
+                    'Content',
+                    'Display'
+                ]
+            },{
+                name: 'UI Components',
+                icon: 'mdi-view-dashboard',
+                lists: [
+                    'API explorer',
+                    'Alerts'
+                ]
+            },{
+                name: 'Directives',
+                icon: 'mdi-function'
+            },{
+                name: 'Preminum themes',
+                icon: 'mdi-vuetify'
+            }]
+        }
+    }
 }
 </script>
 
